dda5cae0232ece2858ff8b515fc33210d9c2c963
bmurdock <brian.j.murdock@gmail.com>
2019-10-24 20:43:55 -0600

    Class progress thus far

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,7 +1,23 @@
-function loadScript(src) {
+function loadScript(src, callback) {
     let script = document.createElement('script');
     script.src = src;
     document.head.append(script);
+    if (typeof callback === 'function') {
+        script.addEventListener('load', function (event) {
+            callback(script);
+        });
+    }
+}
+
+function promiseScript(src) {
+    return new Promise(function (resolve, reject) {
+        let script = document.createElement('script');
+        script.src = src;
+        document.head.append(script);
+        script.addEventListener('load', function (event) {
+            resolve(script);
+        });
+    });
 }
 
 function loadStyle(src) {
