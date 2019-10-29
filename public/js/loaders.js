c988e7bcd8d8addf1fb0f2049def6392435de78a
bmurdock <brian.j.murdock@gmail.com>
2019-10-28 18:24:42 -0600

    Added promise methods

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -20,9 +20,51 @@ function promiseScript(src) {
     });
 }
 
-function loadStyle(src) {
+function loadStyle(src, callback) {
+    // Main code for the load style function.
+    // This gets executed no matter what
     const link = document.createElement('link');
     link.setAttribute('rel', 'stylesheet');
     link.setAttribute('href', src);
     document.head.append(link);
+    // If callback is passed as an argument, then execute the rest
+    if (typeof callback === 'function') {
+        // The file loads succesfully
+        link.addEventListener('load', (event) => {
+            // In a promise, this would be the resolve
+            // The first argument is null to indicate that there is no error
+            callback(null, link);
+        });
+        // The file does not load
+        // the error event is only valid for
+        // <img>, <input type="image">, <object>, <link>, and <script>
+        // We can add it because our link is <link>
+        link.addEventListener('error', (event) => {
+            // In a promise, this would be the reject
+            const err = new Error('Stylesheet failed to load.');
+            callback(err);
+        });
+    }
+}
+function promiseStyle(src) {
+    return new Promise(function (resolve, reject) {
+        const link = document.createElement('link');
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('href', src);
+        document.head.append(link);
+        link.addEventListener('load', (event) => {
+            // In a promise, this would be the resolve
+            // The first argument is null to indicate that there is no error
+            resolve(link);
+        });
+        // The file does not load
+        // the error event is only valid for
+        // <img>, <input type="image">, <object>, <link>, and <script>
+        // We can add it because our link is <link>
+        link.addEventListener('error', (event) => {
+            // In a promise, this would be the reject
+            const err = new Error('Stylesheet failed to load.');
+            reject(err);
+        });
+    })
 }
